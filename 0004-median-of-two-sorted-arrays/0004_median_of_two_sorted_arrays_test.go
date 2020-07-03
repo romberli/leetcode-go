f94af2809415dd95ff4f281c06e07d9c7a7e8284
romber <romber2001@163.com>
2020-07-03 10:35:03 +0800

    0004_median_of_two_sorted_arrays

--- 0004-median-of-two-sorted-arrays/0004_median_of_two_sorted_arrays_test.go
+++ 0004-median-of-two-sorted-arrays/0004_median_of_two_sorted_arrays_test.go
@@ -14,23 +14,23 @@ func TestFindMedianSortedArrays(t *testing.T) {
 
 	asst := assert.New(t)
 
-	// nums1 = []int{1, 3, 5, 7, 9}
-	// nums2 = []int{2, 4}
-	// expect = float64(4)
-	// result = findMedianSortedArrays(nums1, nums2)
-	// asst.Equal(expect, result, "wrong answer.")
-	//
-	// nums1 = []int{1, 2}
-	// nums2 = []int{3, 4}
-	// expect = float64(2.5)
-	// result = findMedianSortedArrays(nums1, nums2)
-	// asst.Equal(expect, result, "wrong answer.")
+	nums1 = []int{1, 3, 5, 7, 9}
+	nums2 = []int{2, 4}
+	expect = float64(4)
+	result = findMedianSortedArrays(nums1, nums2)
+	asst.Equal(expect, result, "wrong answer.")
 
-	// nums1 = []int{1, 2}
-	// nums2 = []int{-1, 3}
-	// expect = float64(1.5)
-	// result = findMedianSortedArrays(nums1, nums2)
-	// asst.Equal(expect, result, "wrong answer.")
+	nums1 = []int{1, 2}
+	nums2 = []int{3, 4}
+	expect = float64(2.5)
+	result = findMedianSortedArrays(nums1, nums2)
+	asst.Equal(expect, result, "wrong answer.")
+
+	nums1 = []int{1, 2}
+	nums2 = []int{-1, 3}
+	expect = float64(1.5)
+	result = findMedianSortedArrays(nums1, nums2)
+	asst.Equal(expect, result, "wrong answer.")
 
 	nums1 = []int{4}
 	nums2 = []int{1, 2, 3, 5}
